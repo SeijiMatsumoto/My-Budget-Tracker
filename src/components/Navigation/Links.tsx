85289a2fa2edf9542ea3e9e86058e01a46259485
SeijiMatsumoto <seijim27@gmail.com>
2023-12-21 14:24:19 -0500

    Make menu collapsible

--- src/components/Navigation/Links.tsx
+++ src/components/Navigation/Links.tsx
@@ -6,25 +6,30 @@ import styles from '../../styles/Navigation/links.module.scss'
 import { links } from '@/data/links'
 import { Link } from '@chakra-ui/next-js';
 
-function Links() {
+interface Props {
+  expanded: boolean;
+}
+
+function Links({ expanded }: Props) {
   const { page, setPage } = useMyNavigationContext();
 
   return (
-    <Flex mt="50px" flexDir={"column"} fontSize={16}>
+    <Flex mt="50px" flexDir={"column"} fontSize={16} className={!expanded ? styles.collapsed : ""}>
       {links.map((link, i) => {
         return (
-          <Flex key={link.title + i} flexDir={"column"} className={styles.category}>
-            <h4 className={styles.heading}>{link.title}</h4>
+          <Flex key={link.title + i} flexDir={"column"} className={!expanded ? styles.collapsed : ""}>
+            {expanded && <h4 className={styles.heading}>{link.title}</h4>}
             {link.children.map((child, j) => {
               return (
                 <Link
                   key={child.title + j}
                   href={child.href}
-                  className={[styles.link, page.toLowerCase() === child.title.toLowerCase() ? styles.active : ""].join(" ")}
+                  className={[styles.link, page.toLowerCase() === child.title.toLowerCase() ? styles.active : "", !expanded ? styles.collapsed : ""].join(" ")}
                   onClick={() => setPage(child.title)}
+                  paddingX={expanded ? "10px" : "0"}
                 >
-                  <Icon as={child.icon} className={styles.icon} />
-                  {child.title}
+                  <Icon as={child.icon} className={expanded ? styles.iconExpanded : styles.iconCollapsed} />
+                  {expanded && child.title}
                 </Link>
               )
             })}
--- src/components/Navigation/SideNav.tsx
+++ src/components/Navigation/SideNav.tsx
@@ -1,21 +1,27 @@
 "use client";
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../../styles/Navigation/sideNav.module.scss'
 import { Heading, Icon } from '@chakra-ui/react';
 import Links from './Links';
 import { GiHummingbird } from "react-icons/gi";
+import { MdArrowBackIosNew, MdArrowForwardIos } from "react-icons/md";
 
-type Props = {}
-
-export default function SideNav({ }: Props) {
+export default function SideNav() {
+  const [expanded, setExpanded] = useState(true);
 
   return (
-    <header className={styles.wrapper}>
+    <header className={[styles.wrapper, expanded ? styles.expanded : styles.collapsed].join(" ")}>
       <Heading size={'md'} mt={"10px"} className={styles.heading}>
         <Icon as={GiHummingbird} className={styles.icon} />
-        CJ Finances
+        {expanded && "CJ Fin"}
       </Heading>
-      <Links />
+      <Links expanded={expanded} />
+      <div
+        className={styles.drawer}
+        onClick={() => setExpanded(!expanded)}
+      >
+        <Icon as={expanded ? MdArrowBackIosNew : MdArrowForwardIos} />
+      </div>
     </header>
   )
 }
