4af138b80e7cd3a6c089aa42d9a73e095dd2f3d8
SeijiMatsumoto <seijim27@gmail.com>
2023-12-21 14:33:31 -0500

    Add skeleton to title

--- src/components/Navigation/TopNav.tsx
+++ src/components/Navigation/TopNav.tsx
@@ -5,6 +5,7 @@ import styles from '../../styles/Navigation/topNav.module.scss'
 import { Flex, Heading } from '@chakra-ui/react'
 import { Image } from '@chakra-ui/react'
 import { Link } from '@chakra-ui/next-js';
+import { Skeleton } from '@chakra-ui/react'
 
 type Props = {}
 
@@ -16,7 +17,11 @@ function TopNav({ }: Props) {
       className={styles.wrapper}
       justifyContent="space-between"
     >
-      <Heading size={"lg"} fontWeight={300} textTransform="uppercase">{page}</Heading>
+      <Heading size={"lg"} fontWeight={300} textTransform="uppercase">
+        <Skeleton isLoaded={page.length}>
+          {page}
+        </Skeleton>
+      </Heading>
       <Link href="/settings">
         <Image src="https://i.imgur.com/2BkToy2.jpeg" alt={''} h={10} w={10} borderRadius={50} objectFit="cover" />
       </Link>
