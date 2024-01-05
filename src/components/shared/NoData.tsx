d778c43b74f3e00943ec34b8c0c4510bedac676b
SeijiMatsumoto <seijim27@gmail.com>
2024-01-05 16:24:30 -0500

    Add nodata

--- src/components/shared/NoData.tsx
+++ src/components/shared/NoData.tsx
@@ -1,12 +1,12 @@
-import { Flex, Icon } from '@chakra-ui/react';
+import { Flex, Icon, Text } from '@chakra-ui/react';
 import React from 'react'
-import { PiSealWarningBold } from "react-icons/pi";
+import { WarningIcon } from '@chakra-ui/icons'
 
 const NoData = () => {
   return (
-    <Flex alignItems="center" justifyContent="center">
-      <Icon as={PiSealWarningBold} />
-      There is no data yet.
+    <Flex alignItems="center" justifyContent="center" height="100%" width="100%">
+      <Icon as={WarningIcon} mr={2} w={4} h={4} />
+      <Text fontSize="16px">There is no data yet.</Text>
     </Flex>
   )
 }
