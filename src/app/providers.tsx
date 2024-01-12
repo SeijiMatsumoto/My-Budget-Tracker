35e1039033a8fdea955ef0b6ba552d0c64a54415
SeijiMatsumoto <seijim27@gmail.com>
2024-01-12 14:54:36 -0500

    remove settingscontext

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,7 +2,6 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { MyNavigationProvider } from '@/contexts/NavigationContext'
 import { MyDataProvider } from '@/contexts/DataContext'
-import { MySettingsProvider } from '@/contexts/SettingsContext'
 import { MyAuthProvider } from '@/contexts/AuthContext'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 const theme = createTheme();
@@ -10,19 +9,17 @@ const theme = createTheme();
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <MyAuthProvider>
-      <MySettingsProvider>
-        <MyNavigationProvider>
-          <MyDataProvider>
-            <ThemeProvider theme={theme}>
-              <ChakraProvider>
-                <main className="main">
-                  {children}
-                </main>
-              </ChakraProvider>
-            </ThemeProvider>
-          </MyDataProvider>
-        </MyNavigationProvider>
-      </MySettingsProvider>
+      <MyNavigationProvider>
+        <MyDataProvider>
+          <ThemeProvider theme={theme}>
+            <ChakraProvider>
+              <main className="main">
+                {children}
+              </main>
+            </ChakraProvider>
+          </ThemeProvider>
+        </MyDataProvider>
+      </MyNavigationProvider>
     </MyAuthProvider>
   )
 }
