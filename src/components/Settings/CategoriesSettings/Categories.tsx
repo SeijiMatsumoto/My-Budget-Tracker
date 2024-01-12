2ecf1fca2125046a9ca73ee02ab34d4a96b5a940
SeijiMatsumoto <seijim27@gmail.com>
2024-01-12 14:54:08 -0500

    Add accordion

--- src/components/Settings/CategoriesSettings/Categories.tsx
+++ src/components/Settings/CategoriesSettings/Categories.tsx
@@ -5,6 +5,13 @@ import {
   Heading,
   useToast,
   Spinner,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box,
+  StylesProvider,
 } from '@chakra-ui/react'
 import React, { useEffect, useState } from 'react'
 import { useMyDataContext } from '@/contexts/DataContext'
@@ -12,17 +19,17 @@ import { Button } from '@mui/material'
 import { setDataInFirestore } from '@/data/useFirebase'
 import { useAuth } from '@/contexts/AuthContext'
 import { returnToast } from '@/utils/returnToast'
+import styles from '@/styles/Settings/settings.module.scss'
 
-interface Budget {
-  title: string;
-  value: number;
+interface Category {
+  type: string;
+  data: string[];
 }
 
 const Categories = () => {
   const toast = useToast();
   const { user, loading } = useAuth();
   const { categoriesData, setCategoriesData } = useMyDataContext();
-
 
   const changeHandler = (value: number, index: number) => {
 
@@ -34,8 +41,39 @@ const Categories = () => {
 
   return (
     <Flex flexDir="column" mb={10}>
-      <Heading size="md" fontWeight="normal" mb={2}>Categories</Heading>
-      <Flex justifyContent="center">
+      <Heading size="md" fontWeight="normal" mb={4}>Categories</Heading>
+      <Flex alignItems="center" flexDir="column">
+        <Flex mb={3} justifyContent="center" width="100%">
+          {
+            categoriesData.length ?
+              <Accordion allowToggle width="100%">
+                {categoriesData.map((categoryType: Category, index: number) => {
+                  return (
+                    <AccordionItem key={categoryType.type + index} flexDir="column">
+                      <h2>
+                        <AccordionButton>
+                          <Box as="span" flex='1' textAlign='left'>
+                            {categoryType.type.slice(0, 1).toUpperCase() + categoryType.type.slice(1)} Categories
+                          </Box>
+                          <AccordionIcon />
+                        </AccordionButton>
+                      </h2>
+                      <AccordionPanel >
+                        {categoryType.data.map((category: string, i: number) => {
+                          return (
+                            <Flex key={category + i} flexDir="column">
+                              {category}
+                            </Flex>
+                          )
+                        })}
+                      </AccordionPanel>
+                    </AccordionItem>
+                  )
+                })}
+              </Accordion>
+              : <Spinner />
+          }
+        </Flex>
         <Button variant="outlined" onClick={submitHandler}>Update Categories</Button>
       </Flex>
     </Flex>
