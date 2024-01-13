984855c9eb62797400de8c5c50e5a845d831cbd6
Seiji Matsumoto <seijim27@gmail.com>
2024-01-13 13:36:16 -0500

    Clean up

--- src/components/Stats/CategoriesList.tsx
+++ src/components/Stats/CategoriesList.tsx
@@ -11,12 +11,11 @@ const CategoriesList = ({ dataPoints }: Props) => {
   dataPoints.sort((a: [string, any], b: [string, any]) => b[1] - a[1]);
 
   return (
-    <Flex width="100%" height="50%" justifyContent="center">
+    <Flex width="100%" height="50%" justifyContent="center" overflow-y="scroll">
       <Flex width="60%" flexDir="column" borderTop='1px solid rgba(0, 0, 0, 0.065)'>
         {dataPoints.slice(1).map((point: [string, number]) => (
           <Flex justifyContent="space-between" alignItems="center" className={styles.itemWrapper}>
             <Flex>
-              {/* <Text width='50px' mr={3} className={styles.percentage}>{point[0]}%</Text> */}
               <Text className={styles.label}>{point[0]}</Text>
             </Flex>
             <Text className={styles.sum}>$ {point[1].toFixed(2)}</Text>
