1b05d9e058694f94218f53309fdec20031693875
SeijiMatsumoto <seijim27@gmail.com>
2024-01-14 14:31:01 -0500

    check if client

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 type StorageValue<T> = T | null;
 
@@ -6,17 +6,23 @@ const useLocalStorage = <T>(
   key: string,
   initialValue: T
 ): [StorageValue<T>, (value: T) => void] => {
-  const storedValue = localStorage.getItem(key);
-  const initial: StorageValue<T> = storedValue
-    ? JSON.parse(storedValue)
-    : initialValue;
+  const isClient = typeof window !== "undefined";
+
+  const storedValue = isClient ? localStorage.getItem(key) : null;
+  const initial: StorageValue<T> =
+    isClient && storedValue ? JSON.parse(storedValue) : initialValue;
 
   const [value, setValue] = useState<StorageValue<T>>(initial);
 
   const setStoredValue = (newValue: T) => {
     setValue(newValue);
-    localStorage.setItem(key, JSON.stringify(newValue));
+    isClient && localStorage.setItem(key, JSON.stringify(newValue));
   };
+
+  useEffect(() => {
+    const storedValue = isClient ? localStorage.getItem(key) : null;
+    setValue(isClient && storedValue ? JSON.parse(storedValue) : initialValue);
+  }, [key, isClient, initialValue]);
 
   return [value, setStoredValue];
 };
